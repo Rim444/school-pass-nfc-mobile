c30e4e9b6d192edbc21cbacd9a1c544fa90dc1f9
Rim444 <Pitirim2.0@mail.ru>
2026-02-10 12:40:58 +0300

    widgets.py
    Added GlassCard and BlurBackground classes for UI enhancements.

--- src/ui/widgets.py
+++ src/ui/widgets.py
@@ -1,8 +1,25 @@
-from kivy.animation import Animation
+from kivymd.uix.card import MDCard
+from kivy.uix.effectwidget import EffectWidget, GaussianBlurEffect
+from kivy.graphics import Color, RoundedRectangle
 
-def fade(widget, d=0.25):
-    widget.opacity = 0
-    Animation(opacity=1, duration=d).start(widget)
+class GlassCard(MDCard):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.radius = [24]
+        self.elevation = 0
+        self.md_bg_color = (1, 1, 1, 0.10)
 
-def press(widget):
-    Animation(scale=0.96, duration=0.05) + Animation(scale=1, duration=0.05)
+        with self.canvas.before:
+            Color(1, 1, 1, 0.18)
+            self.bg = RoundedRectangle(pos=self.pos, size=self.size, radius=self.radius)
+
+        self.bind(pos=self.update_bg, size=self.update_bg)
+
+    def update_bg(self, *_):
+        self.bg.pos = self.pos
+        self.bg.size = self.size
+
+class BlurBackground(EffectWidget):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.effects = [GaussianBlurEffect(radius=12)]
